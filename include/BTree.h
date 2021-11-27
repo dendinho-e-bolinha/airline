4c6f86f38cbbdd68e71c776d645c3b5a9eb46955
Guilherme Almeida <gui1612@sapo.pt>
2021-11-27 19:59:05 +0000

    docs: adds documentation to the BST

--- include/BTree.h
+++ include/BTree.h
@@ -1,29 +1,31 @@
 #ifndef PROJ_1_BTREE_H
 #define PROJ_1_BTREE_H
 
-#include <vector>
+// STL imports
 #include <ostream>
-#include <stack>
 #include <stdexcept>
 
+// Files
 #include "BTreeNode.h"
 
 using namespace std;
 
-
 /**
  * @brief Binary Search tree
- * @tparam T - type of the data stored in each node
+ * @tparam T type of the data stored in each node
  *
  * @details For each node n, n->left->data < n->data && n->right->data >= n->data
  */
 template <class T>
 class BTree {
-
 private:
     BTNode<T> *root;
 
 
+    /**
+     * @brief Destroys a tree
+     * @param node starting node
+     */
     void clear(BTNode<T> *node) {
         if (node) {
             clear(node->left);
@@ -33,6 +35,12 @@ private:
     }
 
 
+    /**
+     * @brief Copies a tree
+     *
+     * @param node starting node
+     * @return Returns the root node of the copy tree
+     */
     BTNode<T> *copyTree(const BTNode<T> *node) const {
         if (node) {
             BTNode<T> *newNode = new BTNode<T>(node->data, node->left, node->right);
@@ -42,6 +50,12 @@ private:
     }
 
 
+    /**
+     * @brief Inserts a new node with the given value on a tree
+     *
+     * @param key value to be inserted
+     * @param node starting node
+     */
     void insert(T key, BTNode<T> *node) {
         if (key < node->data) {
             if (node->left) {
@@ -59,6 +73,14 @@ private:
     }
 
 
+    /**
+     * @brief Finds the first node with a given value on a tree
+     *
+     * @param key value to be searched
+     * @param node starting node
+     *
+     * @return Returns the node that contains the value `key`
+     */
     BTNode<T> *search(T key, BTNode<T> *node) {
         if (node) {
             if (key == node->data)
@@ -73,6 +95,12 @@ private:
     }
 
 
+    /**
+     * @brief Displays the tree data in a Preorder format
+     *
+     * @param out ostream
+     * @param node starting node
+     */
     void printPreorder(ostream &out, const BTNode<T> *node) const {
         if (node) {
             out << node->data << ' ';
@@ -82,6 +110,12 @@ private:
     }
 
 
+    /**
+     * @brief Displays the tree data in a Postorder format
+     *
+     * @param out ostream
+     * @param node starting node
+     */
     void printPostorder(ostream &out, const BTNode<T> *node) const {
         if (node) {
             printPostorder(out, node->left);
@@ -91,6 +125,12 @@ private:
     }
 
 
+    /**
+     * @brief Displays the tree data in a Inorder format
+     *
+     * @param out ostream
+     * @param node starting node
+     */
     void printInorder(ostream &out, const BTNode<T> *node) const {
         if (node) {
             printInorder(out, node->left);
@@ -100,30 +140,63 @@ private:
     }
 
 public:
+
+    /**
+     * @brief default constructor
+     */
     BTree() { root = nullptr; }
+
+
+    /**
+     * @brief Constructs a tree with a single root node that has the given value
+     * @param el value for the root data
+     */
     BTree(const T& el) { root = new BTNode<T>(el); }
+
+    /**
+     * @brief Copy constructor
+     * @param bTree Tree to be copied
+     */
     BTree(const BTree<T>& bTree) { root = copyTree(bTree.root); }
 
 
+    /**
+     * @brief Destructor
+     * @details Deallocates all the memory allocated to the BTree object
+     */
     ~BTree() { clear(); }
 
 
+    /**
+     * @brief Destroys the tree and deallocates all the memory associated to it
+     */
     void clear() {
         clear(root);
         root = nullptr;
     }
 
 
+    /**
+     * @brief Retrieves the root BTNode
+     * @return Returns a pointer to the tree's root
+     */
     BTNode<T> *getRoot() const {
         return root;
-        if(root) return root;
-        throw std::underflow_error("BTree instance must have a not NULL value");
     }
 
 
+    /**
+     * @brief Checks if the tree is empty
+     *
+     * @return Returns a boolean that represents the tree being empty (true) or not (false)
+     */
     bool isEmpty() const { return root == nullptr; }
 
 
+    /**
+     * @brief Inserts a value on the tree
+     * @param key value to be inserted
+     */
     void insert(T key) {
         if (root)
             insert(key, root);
@@ -132,23 +205,48 @@ public:
     }
 
 
+    /**
+     * @brief Searches a value on the key
+     * @param key value to be searched
+     *
+     * @return Returns the first node that has the value `key`
+     */
     BTNode<T> *search(T key) {
         return search(key, root);
     }
 
 
+    /**
+     * @brief Displays the tree data in a Preorder format
+     *
+     * @param out ostream
+     */
     void printPreorder(ostream &out) const {
         printPreorder(out, root);
         out << "\n";
     }
 
 
+    /**
+     *
+     * @param out
+     */
+    /**
+     * @brief Displays the tree data in a Postorder format
+     *
+     * @param out ostream
+     */
     void printPostorder(ostream &out) const {
         printPostorder(out, root);
         out << "\n";
     }
 
 
+    /**
+     * @brief Displays the tree data in an Inorder format
+     *
+     * @param out ostream
+     */
     void printInorder(ostream &out) const {
         printInorder(out, root);
         out << "\n";
--- include/BTreeNode.h
+++ include/BTreeNode.h
@@ -1,11 +1,23 @@
 #ifndef PROJ_1_BTREENODE_H
 #define PROJ_1_BTREENODE_H
 
+/**
+ * @Brief Represents a node of the tree
+ *
+ * @tparam T type of the data stored in each node
+ * @param *left Pointer to the left child node
+ * @param *right Pointer to the right child node
+ */
 template <class T>
 struct BTNode {
     T data;
     BTNode *left, *right;
 
+
+    /**
+     * @brief Constructor for a `BTNode`
+     * @details if `left` or `right` are not passed, it is assumed that the node is a leaf
+     */
     BTNode(const T &data, BTNode *left = nullptr, BTNode *right = nullptr) {
         this->data = data;
         this->left = left;
