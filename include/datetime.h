ff6dd21d26c89c51e40eecfad711334b9ce83dfe
Guilherme Almeida <gui1612@sapo.pt>
2021-12-17 00:44:56 +0000

    fix: :fire: remove merge conflict

--- include/datetime.h
+++ include/datetime.h
@@ -55,25 +55,6 @@ class Datetime: public Date, public Time {
      * @param second A second between 0-59
      */
     Datetime(unsigned int year, unsigned int month, unsigned int day, unsigned int hour, unsigned int minute, unsigned int second);
-
-<<<<<<< HEAD
-=======
-    /**
-     * @brief Converts a datetime instance to a string instance, separated by -
-     * @param datetime Datetime instance
-     * @return A string containing data about the datetime instance provided
-     */
-    static std::string toString(const Datetime &datetime);
-
-    /**
-     * @brief Converts a string to a datetime instance
-     * @param datetime The string to be converted
-     * @return A Datetime instance
-     */
-    static Datetime toDatetime(std::string &datetime);
-
-    bool operator<(const Datetime &rhs) const;
->>>>>>> a688dc1 (feat: adds class Airport)
 };
 
 #endif //AIRLINE_DATETIME_H
